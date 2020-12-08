b918cef02a838e7e94211cdc757234f5e86c2552
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-12-08 22:55:25 +0700

    refactor TODOHandler in route

--- routes/routes.go
+++ routes/routes.go
@@ -6,27 +6,25 @@ import (
 )
 
 func SetupRouter() *gin.Engine {
+
+	controllers.TDC = controllers.TODOHandler()
+
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	{
 		v1.GET("todo", func(c *gin.Context) {
-			controllers.TDC = controllers.TODOHandler()
 			controllers.TDC.GetToDos(c)
 		})
 		v1.POST("todo", func(c *gin.Context) {
-			controllers.TDC = controllers.TODOHandler()
 			controllers.TDC.CreateATodo(c)
 		})
 		v1.GET("todo/:id", func(c *gin.Context) {
-			controllers.TDC = controllers.TODOHandler()
 			controllers.TDC.GetATodo(c)
 		})
 		v1.PUT("todo/:id", func(c *gin.Context) {
-			controllers.TDC = controllers.TODOHandler()
 			controllers.TDC.UpdateATodo(c)
 		})
 		v1.DELETE("todo/:id", func(c *gin.Context) {
-			controllers.TDC = controllers.TODOHandler()
 			controllers.TDC.DeleteATodo(c)
 		})
 	}
